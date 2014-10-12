04f87c4e4c705b3f4b3eebc9bd0f8887b46ab157
Jamie Monserrate <jamiemonserrate@gmail.com>
2014-10-12 14:02:06 +0530

    jMe - Dropping the word markup from the constants as well

--- lib/markups/simple.rb
+++ lib/markups/simple.rb
@@ -1,9 +1,9 @@
 module Markups
   class Simple
-    FLAT_MARKUP_RATE = 0.05
-    FOOD_MARKUP_RATE = 0.13
-    PHARMACEUTICALS_MARKUP_RATE= 0.075
-    ELECTRONICS_MARKUP_RATE= 0.02
+    FLAT_RATE = 0.05
+    FOOD_RATE = 0.13
+    PHARMACEUTICALS_RATE= 0.075
+    ELECTRONICS_RATE= 0.02
 
     def initialize(markup_rate)
       @markup_rate = markup_rate
